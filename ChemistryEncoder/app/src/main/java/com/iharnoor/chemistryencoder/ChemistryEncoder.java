56b4ede8f6f2c9010b9d91f96b343cfbe3439bc4
Harnoor Singh <hsingh9@student.gsu.edu>
2017-11-26 19:14:55 -0500

    Version 2
    Bug Fixes

--- ChemistryEncoder/app/src/main/java/com/iharnoor/chemistryencoder/ChemistryEncoder.java
+++ ChemistryEncoder/app/src/main/java/com/iharnoor/chemistryencoder/ChemistryEncoder.java
@@ -1,11 +1,14 @@
 package com.iharnoor.chemistryencoder;
 
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ChemistryEncoder extends AppCompatActivity {
     EditText key;
@@ -22,6 +25,9 @@ public class ChemistryEncoder extends AppCompatActivity {
         output = (TextView) findViewById(R.id.output);
         Button btn = (Button) findViewById(R.id.encode);
 
+        //enable copy pasting for Output TextView
+        output.setTextIsSelectable(true);
+        //encoding button
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,7 +35,7 @@ public class ChemistryEncoder extends AppCompatActivity {
             }
         });
         Button btn2 = (Button) findViewById(R.id.decode);
-
+        //decoding button
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,17 +43,38 @@ public class ChemistryEncoder extends AppCompatActivity {
             }
         });
 
+        output.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickOuput(v);
+            }
+        });
     }
 
     void encodeButton(View v) {
-        int key2 = Integer.parseInt(key.getText().toString());
-        String encodedMes = encryption.encode(message.getText().toString(), key2);
-        output.setText(encodedMes);
+        if (!isEmpty(key) && !isEmpty(message)) {
+            int key2 = Integer.parseInt(key.getText().toString());
+            String encodedMes = encryption.encode(message.getText().toString(), key2);
+            output.setText(encodedMes);
+        } else output.setText("Please enter both Message and Key");
     }
 
     void decodeButton(View v) {
-        int key2 = Integer.parseInt(key.getText().toString());
-        String decodedMes = encryption.decode(message.getText().toString(), key2);
-        output.setText(decodedMes);
+        if (!isEmpty(key) && !isEmpty(message)) {
+            int key2 = Integer.parseInt(key.getText().toString());
+            String decodedMes = encryption.decode(message.getText().toString(), key2);
+            output.setText(decodedMes);
+        } else output.setText("Please enter both Message and Key");
     }
+
+    void onClickOuput(View view) {
+        ClipboardManager cm = (ClipboardManager) this.getSystemService(Context.CLIPBOARD_SERVICE);
+        cm.setText(output.getText());
+        Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
+    }
+
 }
